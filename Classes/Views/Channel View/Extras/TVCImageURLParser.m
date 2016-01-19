12498e8c0fecb5730742d87fe86e3f549a4f4659
Gerardo <iiigerardoiii@gmail.com>
2016-01-19 11:34:03 -0600

    Added support for 8chan webm thumbnails.
    @donics thx fam

--- Classes/Views/Channel View/Extras/TVCImageURLParser.m
+++ Classes/Views/Channel View/Extras/TVCImageURLParser.m
@@ -229,7 +229,20 @@
             NSString *s = [path substringToIndex:[path length] - 5];
             return [NSString stringWithFormat:@"%@://%@%@s.jpg", scheme, host, s];
         }
-	} else if ([host hasSuffix:@"movapic.com"]) {
+    } else if ([host hasSuffix:@"media.8ch.net"]) {
+        NSObjectIsEmptyAssertReturn(path, nil);
+        if ([path hasSuffix:@".webm"]) {
+            
+            NSString *pathNoExtension = [path substringToIndex:[path length] - 5];
+            
+            NSRange srcLocation = [pathNoExtension rangeOfString:@"src" options:NSBackwardsSearch];
+            if(NSNotFound != srcLocation.location){
+                NSString *replacedSrcPath = [pathNoExtension stringByReplacingCharactersInRange: srcLocation withString: @"thumb"];
+                return [NSString stringWithFormat:@"%@://%@%@.jpg", scheme, host, replacedSrcPath];
+                //printf("REP: %s\n", [result UTF8String]);
+            }
+        }
+    } else if ([host hasSuffix:@"movapic.com"]) {
 		if ([path hasPrefix:@"/pic/"]) {
 			NSString *s = [path substringFromIndex:5];
 
